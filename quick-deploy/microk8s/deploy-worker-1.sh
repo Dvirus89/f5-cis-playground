0d1ba08f03149421f03d5b6d4ab0155ad2fe6aef
dvirus89 <you@example.com>
2025-12-25 21:42:22 +0000

    microk8s

---
+++ quick-deploy/microk8s/deploy-worker-1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+apt-get update -y
+snap install microk8s --classic
+microk8s status -w
+cat /etc/netplan/50-cloud-init.yaml | sed 's/ens5/ens6/g' > /etc/netplan/70-cloud-init.yaml
+netplan apply
+sed -i 's@10.1.0.0/16@10.100.0.0/16@g' /var/snap/microk8s/current/args/cni-network/cni.yaml
+sed -i 's@10.1.0.0/16@10.100.0.0/16@g' /var/snap/microk8s/current/args/kube-proxy
+microk8s kubectl apply -f /var/snap/microk8s/current/args/cni-network/cni.yaml
+microk8s kubectl delete ippools default-ipv4-pool -n kube-system
+microk8s kubectl rollout restart daemonset/calico-node -n kube-system
+microk8s reset
+microk8s start
+microk8s enable dns
+microk8s enable metrics-server
+microk8s enable dashboard
+microk8s enable hostpath-storage
+snap restart microk8s
+
+
+
+
+
